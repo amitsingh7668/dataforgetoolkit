0c0960a32d7b45549ada23d2180c285f0bfcb5be
gh-amsingh <amsingh@guardanthealth.com>
2024-04-17 12:49:30 +0530

    data forge

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dataforgetoolkit
+
+It is a library that facilitates converting CSV files to various formats (such as DataFrames or other CSV/Excel files) based on a JSON mapping,
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='datawizard',
+    version='0.1.0',
+    setup_requires=['wheel'],
+    packages=find_packages(),
+    url='https://github.com/amitsingh7668/datawizard',
+    license='Amit Singh',
+    author='Amit Singh',
+    author_email='2singh.amit3@gmail.com',
+    description='It is a library that facilitates converting CSV files to various formats (such as DataFrames or other CSV/Excel files) based on a JSON mapping',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
